a293c1004ffb74ab95e3d3d6012e848fa79ce5f5
Saurav Avachat <saurav310304@gmail.com>
2026-07-26 15:25:44 +0530

    Implement open-loop simulation in linear_simulator

---
+++ src/simulation/linear_simulator.cpp
@@ -0,0 +1,22 @@
+#include "controls/simulation/linear_simulator.hpp"
+
+namespace controls {
+
+SimulationResult simulateOpenLoop(const StateSpace &system,
+                                   const Eigen::VectorXd &x0,
+                                   const std::vector<Eigen::VectorXd> &controlSequence) {
+    SimulationResult result;
+    Eigen::VectorXd x = x0;
+
+    for (const auto &u : controlSequence) {
+        result.states.push_back(x);
+        result.outputs.push_back(system.output(x, u));
+        result.controls.push_back(u);
+
+        x = system.step(x, u);
+    }
+
+    return result;
+}
+
+}  // namespace controls
